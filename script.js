1ea2afdd799e46f83aa825f2ee54b522ad7f787f
Dava2010 <davlatbek20101106@gmail.com>
2025-04-17 09:50:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let color = prompt('background colorni tanlang')
+
+document.body.style.backgroundColor = `${color}`
+
+let text = prompt('text rangini tanlang')
+
+document.getElementById("text").style.color = `${text}`
